caf750e8f73d6540f54d85ea9ca779c71903d17c
Lavruk Yulia <lavruk_yulia@ukr.net>
2019-08-15 16:31:18 +0300

    task1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import imagesData from "./data/images";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      images: imagesData
+    };
+  }
+  render() {
+    const { images } = this.state;
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-8">
+            <h1>Top commented</h1>
+            <div className="row">
+              {images.map(item => {
+                return (
+                  <div className="col-4">
+                    <div className="card">
+                      <img
+                        className="card-img-top card-img--height"
+                        src={item.url}
+                        alt=""
+                      />
+                      <div className="card-body">
+                        <h6 className="card-title">{item.title}</h6>
+                        <div className="card-text">
+                          Number of comments: {item.comments}
+                        </div>
+                        <a href={item.link}>Link</a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/data/images.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    url:
+      "https://s3.amazonaws.com/talkable-screenshots/Sr._Front-end_developer_test_-_Google_Docs_2016-09-16_18-36-31.png",
+    title: "Giant teddy bear cuddles :)",
+    comments: 1474,
+    link: "#image1"
+  },
+  {
+    id: 2,
+    url:
+      "https://s3.amazonaws.com/talkable-screenshots/Sr._Front-end_developer_test_-_Google_Docs_2016-09-16_18-38-55.png",
+    title: "Again! Again!",
+    comments: 791,
+    link: "#image2"
+  }
+];
